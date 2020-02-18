b77447598fa0d9c772bc58c6351f957f7845e5cc
Osni Pezzini Junior <osni.pezzini@gmail.com>
2020-02-18 00:26:52 -0300

    Add new main call
    - Add initial license check
    - Add initial config check

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-import sys
+import wx
+
 from core import *
 from views.main import MainView
 
 
+def main():
+    import os
+    if os.path.exists('eservices.key'):
+        if os.path.exists('config.db'):
+            widget = MainView()
+            widget.Show()
+        else:
+            from views.config import ConfigView
+            widget = ConfigView()
+            widget.Show()
+    else:
+        from views.license import LicenseView
+        widget = LicenseView()
+        if widget.ShowModal() == wx.OK:
+            main()
+        else:
+            app.Destroy()
+
+
 if __name__ == '__main__':
     app = BaseApp()
-    widget = MainView()
-    widget.Show()
+    main()
     app.MainLoop()
