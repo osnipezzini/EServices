518a3937e5ca7860e4964165787603bc31cf7c68
Osni Pezzini Junior <osni.pezzini@gmail.com>
2020-02-19 00:22:24 -0300

    Add status bar message from license

--- views/main.py
+++ views/main.py
@@ -1,4 +1,5 @@
 from core import get_icon
+from core.license import License
 from ui.main_ui import MainUi
 
 
@@ -9,31 +10,9 @@ class MainView(MainUi):
         self.SetSize((800, 600))
         self.Maximize()
         self.Centre()
-        # from models.person import Person, PersonGroup
-        #
-        # session = DBSession()
-        #
-        # group = PersonGroup(name="CLIENTES")
-        # session.add(group)
-        # session.commit()
-        # session.flush()
-        # print(group.grid)
-        # # # Insert a Person in the person table
-        # new_person = Person(name='Osni Pezzini Jr', email='teste@teste.com', doc='000.000.000-00',
-        #                     city_code=4202404, city_name='Blumenau', state='SC', zipcode='89037-430',
-        #                     district='Água Verde', country='Brasil', address='Rua Martinha Eskelsen', address_nr='45',
-        #                     group_grid=group.grid
-        #                     )
-        # session.add(new_person)
-        # session.commit()
-        # session.flush()
-        # #
-        # product = Product(barcode='0000021212', name='Produto teste', price=10.0, person=new_person)
-        # session.add(product)
-        # session.commit()
-        # session.flush()
-        # for user, product in session.query(Person, Product):
-        #     print(product.person)
+        lic = License().load()
+        self.status_bar.SetStatusText(f"Software registrado para : {lic.Company}")
+
 
     def open_client(self, event):
         from views.create.client import ClientView
