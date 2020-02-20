48a4b992a1bd8640a1e3da050c40c23c7ffaacac
Osni Pezzini Junior <osni.pezzini@gmail.com>
2020-02-20 13:16:59 -0300

    Add fill form method to config view

--- views/config.py
+++ views/config.py
@@ -11,6 +11,12 @@ class ConfigView(ConfigUI):
     def __init__(self, parent=None):
         super(ConfigView, self).__init__(parent)
         self.SetIcon(get_icon())
+        if config.load():
+            self.text_db_host.SetValue(config.db_host)
+            self.text_db_port.SetValue(config.db_port)
+            self.text_db_name.SetValue(config.db_name)
+            self.text_db_username.SetValue(config.db_user)
+            self.text_db_password.SetValue(config.db_password)
 
     def save_cfg(self, event):
 
@@ -30,4 +36,18 @@ class ConfigView(ConfigUI):
 
         cfg = dict(host=host, port=port, name=name, user=user, password=password, driver='PostgreSQL')
         config.save(cfg)
-        wx.MessageBox('Configurações salvas !', 'Sucesso', style=wx.OK | wx.CENTRE | wx.ICON_INFORMATION)
+        self.fill_form(cfg)
+        if wx.MessageBox('Configurações salvas !', 'Sucesso', style=wx.OK | wx.CENTRE | wx.ICON_INFORMATION):
+            self.Destroy()
+
+    def fill_form(self, data):
+        if 'user' in data:
+            self.text_db_username.SetValue(data['user'])
+        if 'port' in data:
+            self.text_db_port.SetValue(data['port'])
+        if 'name' in data:
+            self.text_db_name.SetValue(data['name'])
+        if 'password' in data:
+            self.text_db_password.SetValue(data['password'])
+        if 'host' in data:
+            self.text_db_host.SetValue(data['host'])
