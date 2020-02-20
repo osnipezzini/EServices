43a56885f8e21e6b3f0866955a34854dcbe4a6f7
Osni Pezzini Junior <osni.pezzini@gmail.com>
2020-02-20 13:16:25 -0300

    Protected password field

--- ui/config_ui.py
+++ ui/config_ui.py
@@ -10,98 +10,105 @@
 import wx
 import wx.xrc
 
-
 ###########################################################################
 ## Class ConfigUI
 ###########################################################################
 
-class ConfigUI(wx.Frame):
+class ConfigUI ( wx.Frame ):
 
-    def __init__(self, parent):
-        wx.Frame.__init__(self, parent, id=wx.ID_ANY, title=u"Configurações Locais", pos=wx.DefaultPosition,
-                          size=wx.Size(500, 300), style=wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL)
+	def __init__( self, parent ):
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Configurações Locais", pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
-        self.SetSizeHints(wx.DefaultSize, wx.DefaultSize)
+		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 
-        bSizer1 = wx.BoxSizer(wx.VERTICAL)
+		bSizer1 = wx.BoxSizer( wx.VERTICAL )
 
-        fgSizer1 = wx.FlexGridSizer(0, 4, 0, 0)
-        fgSizer1.AddGrowableCol(1)
-        fgSizer1.SetFlexibleDirection(wx.BOTH)
-        fgSizer1.SetNonFlexibleGrowMode(wx.FLEX_GROWMODE_SPECIFIED)
+		fgSizer1 = wx.FlexGridSizer( 0, 4, 0, 0 )
+		fgSizer1.AddGrowableCol( 1 )
+		fgSizer1.SetFlexibleDirection( wx.BOTH )
+		fgSizer1.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
 
-        self.m_staticText1 = wx.StaticText(self, wx.ID_ANY, u"Endereço IP", wx.DefaultPosition, wx.DefaultSize, 0)
-        self.m_staticText1.Wrap(-1)
+		self.m_staticText1 = wx.StaticText( self, wx.ID_ANY, u"Endereço IP", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText1.Wrap( -1 )
 
-        fgSizer1.Add(self.m_staticText1, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_CENTER_HORIZONTAL, 5)
+		fgSizer1.Add( self.m_staticText1, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
 
-        self.text_db_host = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
-        fgSizer1.Add(self.text_db_host, 0, wx.ALL | wx.EXPAND, 5)
+		self.text_db_host = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		fgSizer1.Add( self.text_db_host, 0, wx.ALL|wx.EXPAND, 5 )
 
-        self.m_staticText2 = wx.StaticText(self, wx.ID_ANY, u"Porta", wx.DefaultPosition, wx.DefaultSize, 0)
-        self.m_staticText2.Wrap(-1)
+		self.m_staticText2 = wx.StaticText( self, wx.ID_ANY, u"Porta", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText2.Wrap( -1 )
 
-        fgSizer1.Add(self.m_staticText2, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL, 5)
+		fgSizer1.Add( self.m_staticText2, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
 
-        self.text_db_port = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
-        fgSizer1.Add(self.text_db_port, 0, wx.ALL, 5)
+		self.text_db_port = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		fgSizer1.Add( self.text_db_port, 0, wx.ALL, 5 )
 
-        bSizer1.Add(fgSizer1, 0, wx.EXPAND, 5)
 
-        bSizer3 = wx.BoxSizer(wx.HORIZONTAL)
+		bSizer1.Add( fgSizer1, 0, wx.EXPAND, 5 )
 
-        self.m_staticText3 = wx.StaticText(self, wx.ID_ANY, u"Nome", wx.DefaultPosition, wx.DefaultSize, 0)
-        self.m_staticText3.Wrap(-1)
+		bSizer3 = wx.BoxSizer( wx.HORIZONTAL )
 
-        bSizer3.Add(self.m_staticText3, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL, 5)
+		self.m_staticText3 = wx.StaticText( self, wx.ID_ANY, u"Nome", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText3.Wrap( -1 )
 
-        self.text_db_name = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
-        bSizer3.Add(self.text_db_name, 1, wx.ALL, 5)
+		bSizer3.Add( self.m_staticText3, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
 
-        bSizer1.Add(bSizer3, 0, wx.EXPAND, 5)
+		self.text_db_name = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer3.Add( self.text_db_name, 1, wx.ALL, 5 )
 
-        bSizer31 = wx.BoxSizer(wx.HORIZONTAL)
 
-        self.m_staticText31 = wx.StaticText(self, wx.ID_ANY, u"Usuário", wx.DefaultPosition, wx.DefaultSize, 0)
-        self.m_staticText31.Wrap(-1)
+		bSizer1.Add( bSizer3, 0, wx.EXPAND, 5 )
 
-        bSizer31.Add(self.m_staticText31, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL, 5)
+		bSizer31 = wx.BoxSizer( wx.HORIZONTAL )
 
-        self.text_db_username = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
-        bSizer31.Add(self.text_db_username, 1, wx.ALL, 5)
+		self.m_staticText31 = wx.StaticText( self, wx.ID_ANY, u"Usuário", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText31.Wrap( -1 )
 
-        bSizer1.Add(bSizer31, 0, wx.EXPAND, 5)
+		bSizer31.Add( self.m_staticText31, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
 
-        bSizer32 = wx.BoxSizer(wx.HORIZONTAL)
+		self.text_db_username = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer31.Add( self.text_db_username, 1, wx.ALL, 5 )
 
-        self.m_staticText32 = wx.StaticText(self, wx.ID_ANY, u"Senha", wx.DefaultPosition, wx.DefaultSize, 0)
-        self.m_staticText32.Wrap(-1)
 
-        bSizer32.Add(self.m_staticText32, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL, 5)
+		bSizer1.Add( bSizer31, 0, wx.EXPAND, 5 )
 
-        self.text_db_password = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
-        bSizer32.Add(self.text_db_password, 1, wx.ALL, 5)
+		bSizer32 = wx.BoxSizer( wx.HORIZONTAL )
 
-        bSizer1.Add(bSizer32, 0, wx.EXPAND, 5)
+		self.m_staticText32 = wx.StaticText( self, wx.ID_ANY, u"Senha", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText32.Wrap( -1 )
 
-        bSizer13 = wx.BoxSizer(wx.VERTICAL)
+		bSizer32.Add( self.m_staticText32, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
 
-        self.btn_save = wx.Button(self, wx.ID_ANY, u"Salvar", wx.DefaultPosition, wx.DefaultSize, 0)
-        bSizer13.Add(self.btn_save, 0, wx.ALL, 5)
+		self.text_db_password = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.TE_PASSWORD )
+		bSizer32.Add( self.text_db_password, 1, wx.ALL, 5 )
 
-        bSizer1.Add(bSizer13, 0, 0, 5)
 
-        self.SetSizer(bSizer1)
-        self.Layout()
+		bSizer1.Add( bSizer32, 0, wx.EXPAND, 5 )
 
-        self.Centre(wx.BOTH)
+		bSizer13 = wx.BoxSizer( wx.VERTICAL )
 
-        # Connect Events
-        self.btn_save.Bind(wx.EVT_BUTTON, self.save_cfg)
+		self.btn_save = wx.Button( self, wx.ID_ANY, u"Salvar", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer13.Add( self.btn_save, 0, wx.ALL, 5 )
 
-    def __del__(self):
-        pass
 
-    # Virtual event handlers, overide them in your derived class
-    def save_cfg(self, event):
-        event.Skip()
+		bSizer1.Add( bSizer13, 0, 0, 5 )
+
+
+		self.SetSizer( bSizer1 )
+		self.Layout()
+
+		self.Centre( wx.BOTH )
+
+		# Connect Events
+		self.btn_save.Bind( wx.EVT_BUTTON, self.save_cfg )
+
+	def __del__( self ):
+		pass
+
+
+	# Virtual event handlers, overide them in your derived class
+	def save_cfg( self, event ):
+		event.Skip()
+
+
