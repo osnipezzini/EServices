07ca41d78edf42fc4ced7f4306e8a3f5b8ac0aa6
Osni Pezzini Junior <osni.pezzini@gmail.com>
2020-02-20 13:19:40 -0300

    Improved main loader

--- main.py
+++ main.py
@@ -10,14 +10,19 @@ def main():
     if License().check() or is_debug():
         from core.config import Config
         if Config().check() or is_debug():
+            app = BaseApp()
             widget = MainView()
             widget.Show()
+            app.MainLoop()
         else:
             from views.config import ConfigView
+            app = wx.App()
             widget = ConfigView()
             widget.Show()
+            app.MainLoop()
     else:
         from views.license import LicenseView
+        app = wx.App()
         widget = LicenseView()
         if widget.ShowModal() == wx.OK:
             main()
@@ -26,10 +31,7 @@ def main():
 
 
 if __name__ == '__main__':
-    app = BaseApp()
     import os
 
     os.environ['HASH_KEY'] = '81HqDtbqAywKSOumSha3BhWNOdQ26slT6K0YaZeZyPs='
-
     main()
-    app.MainLoop()
